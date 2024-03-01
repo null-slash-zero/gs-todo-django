07901467165875b3424ee9857452d43691709625
Robert Wadsworth <robert.wadsworth@gratitudesoftware.com>
2024-02-29 20:12:21 -0500

    feat: starting models
    create a TimeStampedModelMixin

--- gs_todo/apps/todo/models.py
+++ gs_todo/apps/todo/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from common.mixins import TimeStampedModelMixin
 
-# Create your models here.
+class TodoList(TimeStampedModelMixin):
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.name
+
+class TodoItem(TimeStampedModelMixin):
+    description = models.TextField()
+    completed = models.BooleanField(default=False)
+    todo_list = models.ForeignKey(TodoList, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.description
